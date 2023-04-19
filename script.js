e9e7a160d54f3e9edec92ea0414d683bf3acd1a3
Mihail Kucher <mihail.kucher.92@gmail.com>
2023-04-19 16:18:15 +0300

    functions for counting number of visits and avg session length added

--- script.js
+++ script.js
@@ -1,10 +1,44 @@
 const contactBtn = document.querySelector('.contactBtn')
+const startTime = new Date().getTime();
 
 contactBtn.addEventListener('click',function() {
-    alert('coming soon')
+    alert('coming soon');
 });
 
-document.querySelector('.github_link').addEventListener('click', function(){
-    window.open('https://github.com/kvchxg')
-});
+
+function showVisitsNumber(){
+    
+    if (localStorage.getItem('visits_number') == null){
+        localStorage.setItem('visits_number', 1);
+        localStorage.setItem('total session duration', 0);
+        console.log(`This is Your first visit of this web-site`);
+        
+    }else{
+        let visits_number = localStorage.getItem('visits_number');
+        visits_number++;
+        localStorage.setItem('visits_number', visits_number);
+        console.log(`You visited this web-site ${visits_number} time(s)`);
+    }
+
+}
+
+function showAvgTimeOnPage(){
+    let currentTimeOnPage = +(localStorage.getItem('total session duration'));
+    let LastSessionDuration = +(localStorage.getItem('last session duration'));
+    let newTimeOnPage = currentTimeOnPage + LastSessionDuration;
+    let AvgSession =  newTimeOnPage/+(localStorage.getItem('visits_number'));
+    localStorage.setItem('total session duration', newTimeOnPage);
+    console.log(`Your Average session length is ${AvgSession.toFixed(2)} seconds`);
+}
+
+window.addEventListener('beforeunload',function(){
+    let closeTime = new Date().getTime();
+    let sessionDuration = (closeTime-startTime)/1000;
+    this.localStorage.setItem('last session duration',sessionDuration);
+})
+
+showVisitsNumber();
+showAvgTimeOnPage();
+
+
 
